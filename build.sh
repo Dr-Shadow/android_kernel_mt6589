7c076de245b811ff46d0bb053d568e9793742d00
Dr-Shadow <xxdrshadowxx@gmail.com>
2014-04-09 15:04:53 +0200

    Compile with sabermod toolchain 4.8.3 and strip more stuff from modules

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #Export CROSS_COMPILE to point toolchain
 #export CROSS_COMPILE=/home/dr-shadow/faea/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin/arm-eabi-
-export CROSS_COMPILE="ccache /home/jenkins/jobs/F2S-Omnirom/workspace/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin/arm-eabi-"
+#export CROSS_COMPILE="ccache /home/jenkins/jobs/F2S-Omnirom/workspace/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin/arm-eabi-"
+export CROSS_COMPILE="ccache /home/dr-shadow/tmp/arm-eabi-4.8/bin/arm-eabi-"
 
 #Target Product
 export TARGET_KERNEL_PRODUCT=e960
@@ -14,7 +15,7 @@ cp arch/arm/boot/zImage out/$TARGET_KERNEL_PRODUCT/zImage
 
 #Modules part
 make modules
-make INSTALL_MOD_STRIP=1 INSTALL_MOD_PATH=./out/$TARGET_KERNEL_PRODUCT/system INSTALL_MOD_DIR=./out/$TARGET_KERNEL_PRODUCT/system android_modules_install
+make INSTALL_MOD_STRIP=--strip-unneeded INSTALL_MOD_PATH=./out/$TARGET_KERNEL_PRODUCT/system INSTALL_MOD_DIR=./out/$TARGET_KERNEL_PRODUCT/system android_modules_install
 
 #Repack part - You need to set PATH var correctly poiting to a directory with https://github.com/bgcngm/mtk-tools.git and chmod +x mkbootimg
 #You need ramdisk directory too
